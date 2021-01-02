98f18cfd1765c3aad9b2c7e41335f5393a4249fe
benjaBustos <benjaminbustos@hotmail.com>
2021-01-02 00:49:15 -0300

    upload v1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0,0,0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, player.position.z+ offset.z);        
+    }
+}
---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{  
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 10f;
+    bool jump = false;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    public void OnGround(){
+        animator.SetBool("IsGrounded", true);
+        jump = false;
+    }
+    void FixedUpdate(){
+        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        
+        if(Input.GetButtonDown("Jump") || Input.GetButton("Jump")){
+            jump = true;
+            animator.SetBool("IsGrounded", false);
+        }
+        
+    }
+}
